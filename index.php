c38acb594001b6b6a1309874bf398f0b1caeb6d1
David Kingsbury <david@thekingsburys.com>
2015-06-07 23:01:54 -0400

    Fix a lot of bugs and reformatted messages with attachments

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 /**
  * TODO: Channel in ignored file
  * TODO: script output on single line
+ * Don Holt "32.87,-79.97,32.89,-79.92";
+ * Seattle "45.219,-122.325,47.610,-122.107"
+ * Charleston "32.746705,-80.049185,32.899666,-79.834793"
  */
 
 include "lib/slack-api/Slack.php";
@@ -10,12 +13,21 @@ include "lib/incident.php";
 date_default_timezone_set("America/New_York");
 define("LIFESPAN", 600);
 
+# OPTIONS
+# -----------------------------------------------
+
+$optionsPath = "options.json";
+if (!file_exists($optionsPath)) {
+    exit("$optionsPath is needed.\n");
+}
+$options = json_decode(file_get_contents($optionsPath));
+
 # TOKENS
 # -----------------------------------------------
 
 $tokensPath = "tokens.json";
 if (!file_exists($tokensPath)) {
-    exit("$tokensPath is needed with at least 'slack:[yourToken]'.\n");
+    exit("$tokensPath is needed.\n");
 }
 $serviceTokens = json_decode(file_get_contents($tokensPath));
 
@@ -26,7 +38,7 @@ $knownIncidentsPath = "knownIncidents.json";
 /** @var $knownIncidents incident[] */
 $knownIncidents = array();
 if (file_exists($knownIncidentsPath)) {
-    $knownIncidents = json_decode(file_get_contents($knownIncidentsPath), true);
+    $knownIncidents = json_decode(file_get_contents($knownIncidentsPath));
     if ($knownIncidents === null || !is_array($knownIncidents)) {
         echo "Known incidents did not decode correctly\n";
         $knownIncidents = array();
@@ -38,26 +50,22 @@ if (file_exists($knownIncidentsPath)) {
 
 /** @var $activeIncidentsFromService incident[] */
 $activeIncidentsFromService = array();
-// Don Holt "32.87,-79.97,32.89,-79.92";
-// Seattle "45.219,-122.325,47.610,-122.107"
-// Charleston "32.746705,-80.049185,32.899666,-79.834793"
-$latLongBox = "32.746705,-80.049185,32.899666,-79.834793";
 $bingBaseUrl = "http://dev.virtualearth.net/REST/v1/Traffic/Incidents/";
-$bingUrl = $bingBaseUrl . $latLongBox . "/?key=" . getToken("bing",$serviceTokens);
+$bingUrl = $bingBaseUrl . $options->area . "/?key=" . getToken("bing", $serviceTokens);
 $bingJsonResponse = file_get_contents($bingUrl);
 $bingResponse = json_decode($bingJsonResponse);
 foreach ($bingResponse->resourceSets[0]->resources as $rawIncident) {
     $incident = new incident();
-    $incident->id           = $rawIncident->incidentId;
-    $incident->description  = $rawIncident->description;
-    $incident->congestion   = $rawIncident->congestion;
-    $incident->roadClosed   = $rawIncident->roadClosed;
-    $incident->severity     = $rawIncident->severity;
-    $incident->type         = $rawIncident->type;
-    $incident->startTime    = getTimestampFromJsDateMs($rawIncident->start);
-    $incident->endTime      = getTimestampFromJsDateMs($rawIncident->end);
+    $incident->id = $rawIncident->incidentId;
+    $incident->description = $rawIncident->description;
+    $incident->congestion = $rawIncident->congestion;
+    $incident->roadClosed = $rawIncident->roadClosed;
+    $incident->severity = $rawIncident->severity;
+    $incident->type = $rawIncident->type;
+    $incident->startTime = getTimestampFromJsDateMs($rawIncident->start);
+    $incident->endTime = getTimestampFromJsDateMs($rawIncident->end);
     $incident->lastModified = getTimestampFromJsDateMs($rawIncident->lastModified);
-    $incident->lastSeen     = time();
+    $incident->lastSeen = time();
     $activeIncidentsFromService[] = $incident;
 }
 
@@ -69,17 +77,16 @@ $newIncidents = array();
 $updatedIncidents = array();
 foreach ($activeIncidentsFromService as $activeIncidentFromService) {
     $incidentIsNew = true;
-    foreach ($knownIncidents as $knownIncident) {
+    foreach ($knownIncidents as &$knownIncident) {
         if ($activeIncidentFromService->id == $knownIncident->id) {
             if ($activeIncidentFromService->lastModified != $knownIncident->lastModified) {
                 $updatedIncidents[] = array(
                     "old" => $knownIncident,
                     "new" => $activeIncidentFromService
                 );
+                $knownIncident = unserialize(serialize($activeIncidentFromService));
             }
-            else {
-                $incidentIsNew = false;
-            }
+            $incidentIsNew = false;
             $knownIncident->lastSeen = time();
             break;
         }
@@ -89,89 +96,177 @@ foreach ($activeIncidentsFromService as $activeIncidentFromService) {
     }
 }
 
+# CHAT TO SLACK
+# -----------------------------------------------
+
+foreach ($newIncidents as $newIncident) {
+    $attachments = createNewIncidentAttachments($newIncident);
+    $result = postAlert($attachments);
+    if (!$result) {
+        // TODO: Remove this failure from the $newIncidents array so it doesn't get
+        // saved as a known.
+    }
+}
+
+$actualUpdatedIncidentCount = 0;
+foreach ($updatedIncidents as $updatedIncident) {
+    /** @var $old incident */
+    /** @var $new incident */
+    $old = $updatedIncident["old"];
+    $new = $updatedIncident["new"];
+    // Find changed values
+    $propertiesToChat = array();
+    foreach ($new as $key => $newValue) {
+        if ($key == "lastModified" || $key == "lastSeen") {
+            continue;
+        }
+        if ($newValue != $old->$key) {
+            $propertiesToChat[$key] = $newValue;
+        }
+    }
+    if (count($propertiesToChat) < 1) {
+        continue;
+    }
+    $actualUpdatedIncidentCount++;
+    $attachments = createUpdateIncidentAttachments($old, $new, $propertiesToChat);
+    $result = postAlert($attachments);
+}
+
 # KNOWN INCIDENT BOOK KEEPING
 # -----------------------------------------------
 foreach ($newIncidents as $newIncident) {
     $knownIncidents[] = $newIncident;
 }
 $removedIncidentCount = 0;
-for($i=0,$l = count($knownIncidents); $i<$l; $i++) {
+for ($i = 0, $l = count($knownIncidents); $i < $l; $i++) {
     if ($knownIncidents[$i]->lastSeen < time() - LIFESPAN) {
         unset($knownIncidents[$i]);
         $removedIncidentCount++;
     }
 }
+array_values($knownIncidents);
 file_put_contents($knownIncidentsPath, json_encode($knownIncidents));
 
-# CHAT TO SLACK
+# EXIT
 # -----------------------------------------------
 
-foreach ($newIncidents as $newIncident) {
-    $date = getFormattedDate($newIncident->lastModified);
-    $msg = "*NEW* - {$newIncident->description} _($date)_ ";
-    $result = postNewIncident($msg, getToken("slack",$serviceTokens));
-    if ($result["ok"] != 1) {
-       echo "Failed to post incident '{$newIncident->id}'\n";
-    };
+$exitMsg = date("y-m-d H:i:s") . "\n";
+if (count($newIncidents) || $actualUpdatedIncidentCount || $removedIncidentCount) {
+    $exitMsg .=
+        "Active incidents: " . count($knownIncidents) . "\n" .
+        "New incidents: " . count($newIncidents) . "\n" .
+        "Updated incidents: " . $actualUpdatedIncidentCount . "\n" .
+        "Removed incidents: $removedIncidentCount\n";
+}
+exit($exitMsg);
+
+# -----------------------------------------------
+# FUNCTIONS
+# -----------------------------------------------
+
+/**
+ * @var $incident incident
+ * @return attachments
+ */
+function createNewIncidentAttachments($incident)
+{
+    $date = getFormattedDateTime($incident->startTime);
+    $fields = array(
+        array(
+            "title" => "Type",
+            "value" => $incident->type,
+            "short" => true
+        ),
+        array(
+            "title" => "Severity",
+            "value" => $incident->severity,
+            "short" => true
+        )
+    );
+    if ($incident->congestion) {
+        $fields[] = array(
+            "title" => "Congestion",
+            "value" => $incident->congestion,
+            "short" => true
+        );
+    }
+   if ($incident->roadClosed) {
+        $fields[] = array(
+            "title" => "Road Closed",
+            "value" => "Yes, avoid!",
+            "short" => true
+        );
+    }
+    $attachments = array(
+        array(
+            "fallback" => "*NEW _($date)_*\n{$incident->description}",
+            "pretext" => "New traffic alert reported on $date",
+            "title" => $incident->description,
+            "color" => "#FF0000",
+            "mrkdwn_in" => array("pretext"),
+            "fields" => $fields
+        )
+    );
+    return $attachments;
 }
 
-foreach ($updatedIncidents as $updatedIncident) {
-    /** @var $old incident */
-    /** @var $new incident */
-    $old = $updatedIncidents["old"];
-    $new = $updatedIncidents["new"];
-    // Find changed values
-    $propertiesToChat = array();
-    foreach ($new as $key => $newValue) {
-        if ($newValue != $old[$key]) {
-            $propertiesToChat[$key] = $newValue;
-        }
+/**
+ * @var $old incident
+ * @var $new incident
+ * @return attachments
+ */
+function createUpdateIncidentAttachments($old, $new, $changedProperties)
+{
+    $date = getFormattedDateTime($new->lastModified);
+    $time = getFormattedDateTime($new->lastModified, "h:iA");
+    $fields = array();
+    foreach ($changedProperties as $key => $value) {
+        $fields[] = array(
+            "title" => ucfirst($key),
+            "value" => $value,
+            "short" => true
+        );
     }
-    // Format message
-    $date = getFormattedDate($new->lastModified);
-    $msg = "*UPDATE* on {$new->description}\n";
-    foreach ($propertiesToChat as $key => $value) {
-        $msg .= "* *$key*: $value\n";
-    }
-    $msg = trim($msg);
-    // Send
-    $result = postNewIncident($msg, getToken("slack",$serviceTokens));
-    if ($result["ok"] != 1) {
-        echo "Failed to post incident '{$new->id}'\n";
-    };
+    $attachments = array(
+        array(
+            "fallback" => "*UPDATE _($date)_*\n{$old->description}",
+            "pretext" => "*Update* of \"{$old->description}\" at $time",
+            "title" => "The following properties have changed:",
+            "color" => "#FF0000",
+            "mrkdwn_in" => array("pretext"),
+            "fields" => $fields
+        )
+    );
+    return $attachments;
 }
 
-exit(
-    "Active incidents: " . count($knownIncidents) . "\n" .
-    "New incidents: " . count($newIncidents) . "\n" .
-    "Updated incidents: " . count($updatedIncidents) . "\n" .
-    "Removed incidents: $removedIncidentCount\n"
-);
-
-# -----------------------------------------------
-# HELPERS
-# -----------------------------------------------
-
-function postNewIncident($msg, $token) {
-    $slack = new Slack($token);
+function postAlert($attachments)
+{
+    global $options, $serviceTokens;
+    $slack = new Slack(getToken("slack", $serviceTokens));
     $results = $slack->call(
         "chat.postMessage",
         array(
-            "channel" => "#bot-dev",
+            "channel" => $options->channel,
             "username" => "Charleston Traffic Alerts",
-            "icon_url" => "http://itblog.sandisk.com/wp-content/uploads/2014/02/Pommi_Traffic_Sign.png",
             "icon_emoji" => ":vertical_traffic_light:", //construction, vertical_traffic_light, rotating_light
-            "text" => $msg
+            "attachments" => json_encode($attachments)
         )
     );
-    return $results;
+    if (!$results["ok"]) {
+        echo "Failed to post message: '{$results["error"]}'\n";
+        return false;
+    }
+    return true;
 }
 
-function getFormattedDate($timestamp, $format="H:i:s") {
+function getFormattedDateTime($timestamp, $format = "D. M. jS, h:iA")
+{
     return date($format, $timestamp);
 }
 
-function getToken($serviceName, $serviceTokens) {
+function getToken($serviceName, $serviceTokens)
+{
     $token = false;
     foreach ($serviceTokens as $serviceToken) {
         if ($serviceToken->serviceName == $serviceName) {
@@ -181,6 +276,7 @@ function getToken($serviceName, $serviceTokens) {
     return $token;
 }
 
-function getTimestampFromJsDateMs($rawTimestamp) {
-    return intval(substr($rawTimestamp,6,-5));
+function getTimestampFromJsDateMs($rawTimestamp)
+{
+    return intval(substr($rawTimestamp, 6, -5));
 }
